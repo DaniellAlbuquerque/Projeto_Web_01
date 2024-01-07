94ab7c6cb6d5cf76d48ed13d2bed68a275232e4a
DaniellAlbuquerque <danieljca3@gmail.com>
2024-01-07 03:07:49 -0300

    Add commit

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 alert('Seja bem vindo!')
-
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "/img/close_white_36dp.svg";
-    }
-}
-
 function submitForm() {
     var nome = document.getElementById('nome').value;
     var email = document.getElementById('email').value;
